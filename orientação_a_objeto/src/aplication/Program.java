9063b32daa777be1e3a1bb92dfc2d982a00e1f4e
Otávio Pucharelli <otaviog.m.p@gmail.com>
2023-11-12 07:18:04 -0300

    feat: primeiro exemplo

---
+++ orientação_a_objeto/src/aplication/Program.java
@@ -0,0 +1,38 @@
+package aplication;
+
+import entities.Triangle;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        Triangle x, y;
+        
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Enter the measures of triangle X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Enter the measures of triangle Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.println("Triangle X area: " + areaX);
+        System.out.println("Triangle Y area: " + areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        } else {
+            System.out.println("Larger area: Y");
+        }
+
+        sc.close();
+    }
+}

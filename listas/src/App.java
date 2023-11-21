90dc56bc4a0948b584efb86dd7dcd49d87882200
Otávio Pucharelli <otaviog.m.p@gmail.com>
2023-11-21 11:28:25 -0300

    feat: primeiro programa usando lista

--- listas/src/App.java
+++ listas/src/App.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<String> lista = new ArrayList<String>();
+        lista.add("Ola");
+        lista.add("Mundo"); 
+
+        System.out.println(lista.get(0));
+        System.out.println(lista.get(1));
     }
 }
+
